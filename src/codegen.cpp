0efa27cfd7513b19c76c7ce8126c97e6f7d9316a
mnmaita <47983254+mnmaita@users.noreply.github.com>
2021-03-08 23:05:50 -0300

    Stylistic changes to codegen.cpp * Removed unnecesary whitespace and linebreaks. * Added curly brackets to all if/else statements. * Simplified some comments. * Removed obsolete and unnecesary comments.

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -4,7 +4,6 @@
 #include "log.h"
 #include "symtable.h"
 #include "types.h"
-
 #include <cstring>
 
 #define ASM_HEADER "cabecera.txt"
@@ -19,8 +18,9 @@ int TotalGotos = 0;
 int posInGotos(int pos) {
     int i = 0;
     while (i < TotalGotos) {
-        if (GotoPosition[i] == pos)
+        if (GotoPosition[i] == pos) {
             return 1;
+        }
         i++;
     }
     return 0;
@@ -29,9 +29,10 @@ int posInGotos(int pos) {
 int asmGenerateHeader() {
     FILE* pfASM;
     char buf[50];
-    if ((pfASM = fopen(ASM_HEADER, "w")) == NULL)
+    if ((pfASM = fopen(ASM_HEADER, "w")) == NULL) {
         logError(
             "[Generar Encabezado Assembler]: Error al generar el archivo de encabezado assembler");
+    }
     fprintf(pfASM, "include src/asm/macros2.asm\ninclude src/asm/number.asm\n\n.MODEL "
                    "LARGE\n.STACK 200h\n.386\n\n.DATA\nMAXSTRSIZE equ 33\nTRUE dd 1\nFALSE dd "
                    "0\n@auxStr db MAXSTRSIZE dup (?), '$'\n");
@@ -51,8 +52,9 @@ int asmGenerateHeader() {
 
 int asmGenerateCode() {
     FILE* pfASM;
-    if ((pfASM = fopen(ASM_CODE, "w")) == NULL)
+    if ((pfASM = fopen(ASM_CODE, "w")) == NULL) {
         logError("[Generar Codigo Assembler]: Error al generar el archivo de codigo assembler");
+    }
     fprintf(pfASM, "\n.CODE\n.startup\n\nmov AX,@DATA\nmov DS,AX\nFINIT\n\n");
     fclose(pfASM);
     return 1;
@@ -65,14 +67,17 @@ int asmWrite() {
     char data[50];
     char filebuffer[4097];
     int n, k;
-    if ((pfASM = fopen(ASM_OUT, "w")) == NULL)
+    if ((pfASM = fopen(ASM_OUT, "w")) == NULL) {
         logError("[Grabar Assembler]: Error al generar el archivo assembler final");
-    if ((pfCAB_ASM = fopen(ASM_HEADER, "a")) == NULL)
+    }
+    if ((pfCAB_ASM = fopen(ASM_HEADER, "a")) == NULL) {
         logError("[Grabar Assembler]: Error al querer editar el archivo cabecera assembler");
-    if ((pfCOD_ASM = fopen(ASM_CODE, "a")) == NULL)
+    }
+    if ((pfCOD_ASM = fopen(ASM_CODE, "a")) == NULL) {
         logError("[Grabar Assembler]: Error al querer editar el archivo codigo assembler");
+    }
 
-    /*Recorrer Polaca y transformar a assembler*/
+    // Recorrer Polaca y transformar a assembler
     char* aux1;
     char* aux2;
     int pos = 1;
@@ -83,36 +88,35 @@ int asmWrite() {
         }
 
         if (strcmp(data, "TRUE") == 0) {
-            // popStack(&polish);
             free(popStack(&polish));
             pushStack(&ASMElementsRead, "@aux");
             fprintf(pfCOD_ASM, "fld TRUE\n");
         } else if (strcmp(data, "FALSE") == 0) {
-            // popStack(&polish);
             free(popStack(&polish));
             pushStack(&ASMElementsRead, "@aux");
             fprintf(pfCOD_ASM, "fld FALSE\n");
         } else if (strstr(data, "GOTO") != NULL) {
-            // popStack(&polish);
             free(popStack(&polish));
             fprintf(pfCOD_ASM, "%s: \n", data);
         } else if (strcmp(data, "NOT") == 0 || strcmp(data, "AND") == 0) {
-            free(popStack(&polish)); // Ignoro el AND y OR, pues no generan codigo assembler
-            // popStack(&polish);
-        }
-        // Operadores Binarios
-        else if (strcmp(data, "=") == 0 || strcmp(data, "+") == 0 || strcmp(data, "-") == 0 ||
-                 strcmp(data, "*") == 0 || strcmp(data, "/") == 0 || strcmp(data, "CMP") == 0 ||
-                 strcmp(data, "OR") == 0 || strcmp(data, "++") == 0) {
-            // popStack(&polish); //Quitar el operador de la lista
+            // Ignoro el AND y NOT, pues no generan codigo assembler
             free(popStack(&polish));
-            aux1 = dequeue(&ASMElementsRead); // Consumir dos operandos
+        } else if (strcmp(data, "=") == 0 || strcmp(data, "+") == 0 || strcmp(data, "-") == 0 ||
+                   strcmp(data, "*") == 0 || strcmp(data, "/") == 0 || strcmp(data, "CMP") == 0 ||
+                   strcmp(data, "OR") == 0 || strcmp(data, "++") == 0) {
+            // Operadores Binarios
+            // Quitar el operador de la lista
+            free(popStack(&polish));
+            // Consumir dos operandos
+            aux1 = dequeue(&ASMElementsRead);
             aux2 = dequeue(&ASMElementsRead);
             if (strcmp(data, "CMP") == 0) {
-                if (strcmp(aux1, "@aux") != 0)
+                if (strcmp(aux1, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux1);
-                if (strcmp(aux2, "@aux") != 0)
+                }
+                if (strcmp(aux2, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux2);
+                }
                 fprintf(pfCOD_ASM, "fcomp\nfstsw ax\nfwait\nsahf\n", aux2, aux1);
             } else if (strcmp(data, "=") == 0) {
                 if ((symtableSearchInternalType(aux2) == TYPE_STRING ||
@@ -139,33 +143,42 @@ int asmWrite() {
                             "di,OFFSET _%s\ncall COPIAR\n",
                             aux1);
                 } else {
-                    if (strcmp(aux2, "@aux") != 0)
+                    if (strcmp(aux2, "@aux") != 0) {
                         fprintf(pfCOD_ASM, "fld _%s\n", aux2);
+                    }
                     fprintf(pfCOD_ASM, "fstp _%s\n", aux1);
                 }
             } else if (strcmp(data, "+") == 0) {
-                if (strcmp(aux2, "@aux") != 0)
+                if (strcmp(aux2, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux2);
-                if (strcmp(aux1, "@aux") != 0)
+                }
+                if (strcmp(aux1, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux1);
+                }
                 fprintf(pfCOD_ASM, "fadd\n", aux1);
             } else if (strcmp(data, "-") == 0) {
-                if (strcmp(aux2, "@aux") != 0)
+                if (strcmp(aux2, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux2);
-                if (strcmp(aux1, "@aux") != 0)
+                }
+                if (strcmp(aux1, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux1);
+                }
                 fprintf(pfCOD_ASM, "fsub\n", aux1);
             } else if (strcmp(data, "*") == 0) {
-                if (strcmp(aux2, "@aux") != 0)
+                if (strcmp(aux2, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux2);
-                if (strcmp(aux1, "@aux") != 0)
+                }
+                if (strcmp(aux1, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux1);
+                }
                 fprintf(pfCOD_ASM, "fmul\n", aux1);
             } else if (strcmp(data, "/") == 0) {
-                if (strcmp(aux2, "@aux") != 0)
+                if (strcmp(aux2, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux2);
-                if (strcmp(aux1, "@aux") != 0)
+                }
+                if (strcmp(aux1, "@aux") != 0) {
                     fprintf(pfCOD_ASM, "fld _%s\n", aux1);
+                }
                 fprintf(pfCOD_ASM, "fdiv\n", aux1);
             } else if (strcmp(data, "++") == 0) {
                 fprintf(
@@ -174,8 +187,8 @@ int asmWrite() {
                     "@auxStr\ncall COPIAR\nmov si,OFFSET _%s\nmov di,OFFSET @auxStr\ncall CONCAT\n",
                     aux2, aux1);
             } else if (strcmp(data, "OR") == 0) {
-                // TODO ver que accion tomo ante un OR en la polish, posiblemente debería ignorarlo
-                // al igual que el AND y NOT
+                // TODO: ver que accion tomar ante un OR en la polish, posiblemente
+                // ignorarlo igual que AND y NOT
             }
             // Apilo el auxiliar correspondiente
             if (strcmp(data, "+") == 0 || strcmp(data, "-") == 0 || strcmp(data, "*") == 0 ||
@@ -184,17 +197,14 @@ int asmWrite() {
             }
             free(aux1);
             free(aux2);
-        }
-        // Operadores Unarios
-        else if (strcmp(data, "JL") == 0 || strcmp(data, "JLE") == 0 || strcmp(data, "JG") == 0 ||
-                 strcmp(data, "JGE") == 0 || strcmp(data, "JZ") == 0 || strcmp(data, "JNZ") == 0 ||
-                 strcmp(data, "JMP") == 0 || strcmp(data, "WRITE") == 0 ||
-                 strcmp(data, "READ") == 0) {
+        } else if (strcmp(data, "JL") == 0 || strcmp(data, "JLE") == 0 || strcmp(data, "JG") == 0 ||
+                   strcmp(data, "JGE") == 0 || strcmp(data, "JZ") == 0 ||
+                   strcmp(data, "JNZ") == 0 || strcmp(data, "JMP") == 0 ||
+                   strcmp(data, "WRITE") == 0 || strcmp(data, "READ") == 0) {
+            // Operadores Unarios
             free(popStack(&polish));
-            // popStack(&polish);
             aux1 = dequeue(&ASMElementsRead);
-            // VER WIKIPEDIA https://en.wikipedia.org/wiki/Branch_(computer_science) PARA MAS INFO
-            // SOBRE LOS BRANCH
+            // Link util sobre branches: https://en.wikipedia.org/wiki/Branch_(computer_science)
             if (strcmp(data, "JL") == 0) {
                 fprintf(pfCOD_ASM, "JB GOTO%s\n", aux1);
             } else if (strcmp(data, "JLE") == 0) {
@@ -245,7 +255,6 @@ int asmWrite() {
             }
             free(aux1);
         } else {
-            // printf("Apilar - ASMElementsRead: %s\n", data);
             pushStack(&ASMElementsRead, popStack(&polish));
         }
         pos++;
@@ -259,20 +268,23 @@ int asmWrite() {
     fclose(pfCAB_ASM);
 
     FILE* pfCabecera = fopen(ASM_HEADER, "r");
-    if (pfCabecera == NULL)
+    if (pfCabecera == NULL) {
         logError("[Grabar Assembler]: Error al abrir el archivo cabecera assembler");
+    }
     FILE* pfCodigo = fopen(ASM_CODE, "r");
-    if (pfCodigo == NULL)
+    if (pfCodigo == NULL) {
         logError("[Grabar Assembler]: Error al abrir el archivo codigo assembler");
-
+    }
     while ((n = fread(filebuffer, sizeof(char), 4096, pfCabecera))) {
-        if (!(k = fwrite(filebuffer, sizeof(char), n, pfASM)))
+        if (!(k = fwrite(filebuffer, sizeof(char), n, pfASM))) {
             return -1;
+        }
     }
 
     while ((n = fread(filebuffer, sizeof(char), 4096, pfCodigo))) {
-        if (!(k = fwrite(filebuffer, sizeof(char), n, pfASM)))
+        if (!(k = fwrite(filebuffer, sizeof(char), n, pfASM))) {
             return -1;
+        }
     }
 
     fclose(pfCabecera);
@@ -281,11 +293,13 @@ int asmWrite() {
     remove(ASM_CODE);
     fprintf(pfASM, "mov AX, 4C00h\nint 21h\n\n");
     FILE* pfConcat = fopen("src/asm/concat.asm", "r");
-    if (pfConcat == NULL)
+    if (pfConcat == NULL) {
         logError("[Grabar Assembler]: Error al abrir el archivo concat assembler");
+    }
     while ((n = fread(filebuffer, sizeof(char), 4096, pfConcat))) {
-        if (!(k = fwrite(filebuffer, sizeof(char), n, pfASM)))
+        if (!(k = fwrite(filebuffer, sizeof(char), n, pfASM))) {
             return -1;
+        }
     }
     fprintf(pfASM, "\nEND\n");
     fclose(pfASM);
